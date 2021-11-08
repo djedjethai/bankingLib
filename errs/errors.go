71b506fbfb7ec08e53b903cd9bf629bcef3e919f
Jerome Bidault <djedje.thai.ok@gmail.com>
2021-11-08 20:03:59 +0700

    first commit

---
+++ errs/errors.go
@@ -0,0 +1,58 @@
+package errs
+
+import "net/http"
+
+type AppError struct {
+	Code    int    `json:",omitempty"` // if empty won't appear in json format
+	Message string `json:"message"`
+}
+
+// func to return only the err message
+// can call this function in the handlers/http port
+func (e *AppError) AsMessage() *AppError {
+	return &AppError{
+		Message: e.Message,
+	}
+}
+
+func NewBadRequestError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusBadRequest,
+		Message: message,
+	}
+}
+
+func NewNotFoundError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusNotFound,
+		Message: message,
+	}
+}
+
+func NewInternalServerError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusInternalServerError,
+		Message: message,
+	}
+}
+
+func NewValidationError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusUnprocessableEntity,
+		Message: message,
+	}
+}
+
+func NewAuthenticationError(message string) *AppError {
+	return &AppError{
+		Message: message,
+		Code:    http.StatusUnauthorized,
+	}
+}
+
+func NewAuthorizationError(message string) *AppError {
+	return &AppError{
+		Message: message,
+		Code:    http.StatusForbidden,
+	}
+}
